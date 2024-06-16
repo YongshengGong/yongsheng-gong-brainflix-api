dd40e24d764f9ed62101c7dc4f2667afda7436d1
Yongsheng <ygong56@uwo.ca>
2024-06-16 00:11:54 -0400

    Add basic upload function yet with some modification to do

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/videos.js";
-const app=express();
+const app = express();
 app.use(express.static('./public'));
 app.use(cors());
-const api_key = process.env.api_key||"3e1b084f-72fa-4a65-8938-76c9f8b3a923";
-app.use((req,res,next)=>{
-    if(req.query.api_key!=api_key||!req.query.api_key){
-        res.status(401).json({message:"unauthorized: api key is missing or invalid"});
+app.use(express.json());
+const api_key = process.env.api_key || "3e1b084f-72fa-4a65-8938-76c9f8b3a923";
+app.use((req, res, next) => {
+    if (req.query.api_key != api_key || !req.query.api_key) {
+        res.status(401).json({ message: "unauthorized: api key is missing or invalid" });
         return;
     }
-    else{
+    else {
         // res.status(200).json({message:"api key is valid"});
         next();
     }
 })
-app.use("/videos",router);
-app.listen(8080,()=>{
+app.use("/videos", router);
+app.listen(8080, () => {
     console.log("server is now running on port 8080");
 })
--- routes/videos.js
+++ routes/videos.js
@@ -1,22 +1,32 @@
 import express from "express";
 import fs from "fs";
-const app=express();
-const router=express.Router();
+const app = express();
+const router = express.Router();
 router.route("/")
-      .get((_req,res)=>{
-        let data=JSON.parse(fs.readFileSync("./data/videos.json"));
-        const partialData=data.map(obj=>{
-            const {id,title,channel,image,...rest}=obj;
-            return {id,title,channel,image};
-        })
-         res.json(partialData)
+      .get((_req, res) => {
+            let data = JSON.parse(fs.readFileSync("./data/videos.json"));
+            const partialData = data.map(obj => {
+                  const { id, title, channel, image, ...rest } = obj;
+                  return { id, title, channel, image };
+            })
+            res.json(partialData)
+      })
+      .post((req, res) => {
+            try {
+                  let data = JSON.parse(fs.readFileSync("./data/videos.json"));
+                  data.push(req.body)
+                  fs.writeFileSync("./data/videos.json", JSON.stringify(data));
+            }
+            catch (error) {
+                  console.log(error)
+            }
       })
 
 router.route('/:id')
-      .get((req,res)=>{
-        let data=JSON.parse(fs.readFileSync("./data/videos.json"));
-        const oneObj=data.find(obj=>obj.id===req.params.id);
-        res.json(oneObj)
+      .get((req, res) => {
+            let data = JSON.parse(fs.readFileSync("./data/videos.json"));
+            const oneObj = data.find(obj => obj.id === req.params.id);
+            res.json(oneObj)
       })
 
 export default router;
